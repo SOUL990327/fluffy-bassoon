69933ffdfad852f46ac0a1599192b011fd1b7090
hkmd2024 <mydogiscereberus@gmail.com>
2025-07-06 15:14:19 +0530

    full app with seo 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ← this line is important!
-  plugins: [react(), eslintPlugin()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
-    },
-  },
-});
+  plugins: [react(), tailwindcss(),
+  ],
+})
